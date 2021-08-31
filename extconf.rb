3ad50deefce5ac5ca06dfe837bfdd6ae3089182c
cielavenir <cielartisan@gmail.com>
2021-08-31 14:30:29 +0900

    fix extconf

--- extconf.rb
+++ extconf.rb
@@ -1,6 +1,8 @@
 require 'bundler/setup'
-require 'rice'
+#require 'rice'
 require 'mkmf-rice'
 
+$srcs = ['ext/rbslz.cpp', 'ext/libslz/src/slz.c']
+$VPATH << 'ext' << 'ext/libslz/src'
 create_makefile('slz')
 
